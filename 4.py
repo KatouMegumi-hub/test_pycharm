881b2c05f045f4586b0d70b2fa328c1b6d9dda6c
chen <chen@163.com>
2025-07-20 15:14:16 +0800

    新增一个希尔排序法

---
+++ 4.py
@@ -0,0 +1,19 @@
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and temp < arr[j - gap]:
+                arr[j] = arr[j - gap]
+                j -= gap
+
+            arr[j] = temp
+
+        gap //= 2
+
+    return arr
+
+
+print(shell_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))
